7f7403d5dc09584b17dcff4cfe593120cd337e48
SOUMEN PRADHAN <124460362+SOUMENpradhan01428@users.noreply.github.com>
2025-03-09 14:31:47 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://localhost:3001"], // ✅ Allow both frontend URLs
+        origin: "*", // ✅ Allow both frontend URLs
         credentials: true, // ✅ Allow cookies (for authentication)
     })
 );
